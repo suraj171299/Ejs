1768b239ece4ff14728c536d24365229fdf52cc0
suraj171299 <surajpadalkar1999@gmail.com>
2023-09-10 11:03:00 +0530

    first commit

---
+++ models/product.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const path = require("path");
+const rootDir = require("../util/path");
+
+module.exports = class Product {
+    constructor(t) {
+        this.title = t;
+    }
+
+    save() {
+        const p = path.join(rootDir, "data", "products.JSON");
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
+    }
+
+    static fetchAll() {
+        const p = path.join(rootDir, "data", "products.JSON");
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                return [];
+            }
+            return JSON.parse(fileContent);
+        });
+    }
+};
